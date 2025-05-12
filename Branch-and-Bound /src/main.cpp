47d4b714c8fce5f28978585798a47a09731c2237
Adriane Lopes <adriane@q8.logq.int>
2025-05-12 20:49:15 -0300

    subtour

--- Branch-and-Bound /src/main.cpp
+++ Branch-and-Bound /src/main.cpp
@@ -1,40 +1,60 @@
-#include <iostream>
-using namespace std;
-
 #include "Data.h"
 #include "hungarian.h"
+#include <iostream>
+#include <vector>
+using namespace std;
 
-int main(int argc, char **argv)
-{
+struct Node {
+  vector<pair<int, int>> forbidden_arcs;
+  vector<vector<int>> subtour;
+  double lower_bound; // custo total da solucao do algoritmo hungaro
+  int chosen;         // indice do menor subtour
+  bool feasible;      // indica se a solucao do AP_TSP e viavel
+};
 
-	Data *data = new Data(argc, argv[1]);
-	data->read();
+struct Node Subtours(hungarian_problem_t *p) {}
 
-	double **cost = new double *[data->getDimension()];
-	for (int i = 0; i < data->getDimension(); i++)
-	{
-		cost[i] = new double[data->getDimension()];
-		for (int j = 0; j < data->getDimension(); j++)
-		{
-			cost[i][j] = data->getDistance(i, j);
-		}
-	}
+void updateNode(Node *node, Data *data, double **cost, hungarian_problem_t p) {
+  int mode = HUNGARIAN_MODE_MINIMIZE_COST;
+  hungarian_init(&p, cost, data->getDimension(), data->getDimension(),
+                 mode); // Carregando o problema
+  node->lower_bound = hungarian_solve(&p);
 
-	hungarian_problem_t p;
-	int mode = HUNGARIAN_MODE_MINIMIZE_COST;
-	hungarian_init(&p, cost, data->getDimension(), data->getDimension(), mode); // Carregando o problema
+  /* node->subtour = Subtours(p); // detectar o conjunto de subtours
 
-	double obj_value = hungarian_solve(&p);
-	cout << "Obj. value: " << obj_value << endl;
+  node->chosen = getChoosen(p); // pegar o indice do menor subtour
 
-	cout << "Assignment" << endl;
-	hungarian_print_assignment(&p);
+  node->feasible = isFeasible(p); // verificar viabilidade */
 
-	hungarian_free(&p);
-	for (int i = 0; i < data->getDimension(); i++)
-		delete[] cost[i];
-	delete[] cost;
-	delete data;
+  hungarian_free(&p);
+}
 
-	return 0;
+void branch_and_bound() {}
+
+int main(int argc, char **argv) {
+
+  Data *data = new Data(argc, argv[1]);
+  data->read();
+
+  double **cost = new double *[data->getDimension()];
+  for (int i = 0; i < data->getDimension(); i++) {
+    cost[i] = new double[data->getDimension()];
+    for (int j = 0; j < data->getDimension(); j++) {
+      cost[i][j] = data->getDistance(i, j);
+    }
+  }
+  hungarian_problem_t p;
+  double obj_value = hungarian_solve(&p);
+  cout << "Obj. value: " << obj_value << endl;
+
+  cout << "Assignment" << endl;
+  hungarian_print_assignment(&p);
+
+  hungarian_free(&p);
+  for (int i = 0; i < data->getDimension(); i++)
+    delete[] cost[i];
+  delete[] cost;
+  delete data;
+
+  return 0;
 }
