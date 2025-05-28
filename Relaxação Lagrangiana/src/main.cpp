fffc9550d2be1152fb5fd72116c1c698fed9bb41
Adriane Lopes <adrianelf04@gmail.com>
2025-05-28 14:27:39 -0300

    tentando solucionar problemas

--- Relaxação Lagrangiana/src/main.cpp
+++ Relaxação Lagrangiana/src/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "Kruskal.h"
 #include "Data.h"
+#include <limits>
 
 struct Tree
 {
@@ -11,7 +12,9 @@ struct Tree
 
 vector<vector<double>> modifica_Matriz(Data *data)
 {
-    vector<vector<double>> matriz;
+    int dim = data->getDimension() - 1;
+    cout << "DIM: " << dim << endl;
+    vector<vector<double>> matriz(dim, vector<double>(dim));
     for (int i = 1; i < data->getDimension(); i++)
     {
         for (int j = 1; j < data->getDimension(); j++)
@@ -26,20 +29,23 @@ Tree Make1_Tree(Data &data)
 {
     vector<vector<double>> matriz = modifica_Matriz(&data);
     auto kruskal = Kruskal(matriz);
+    double inicost = kruskal.MST(data.getDimension() - 1);
     Tree tree;
     tree.listAdj = kruskal.getEdges();
-    double inicost = kruskal.MST(data.getDimension() - 1);
+
+    std::cout << "Tamanho listAdj: " << tree.listAdj.size() << std::endl;
+
     double cost;
-    double second = 0;
-    double first;
-    int secondJ;
-    int firstJ = 1;
+    double second = std::numeric_limits<double>::infinity();
+    double first = std::numeric_limits<double>::infinity();
+    int firstJ = -1; // Inicializa índice inválido para detectar erros
+    int secondJ = -1;
 
     for (int j = 1; j < data.getDimension() - 1; j++)
     {
         cost = data.getDistance(0, j);
 
-        if ((cost < first) || j == 1)
+        if (cost < first)
         {
             second = first;
             secondJ = firstJ;
@@ -49,20 +55,36 @@ Tree Make1_Tree(Data &data)
         else if (cost < second)
         {
             second = cost;
+            secondJ = j;
         }
+    }
+
+    std::cout << "firstJ: " << firstJ << ", secondJ: " << secondJ << std::endl;
+
+    int n = data.getDimension();
+    // Redimensionar listAdj para garantir espaço para o vértice 0
+    if ((int)tree.listAdj.size() < n)
+    {
+        tree.listAdj.resize(n);
     }
 
     tree.cost = inicost + first + second;
     vector<int> J = {firstJ, secondJ};
+    tree.listAdj.insert(tree.listAdj.begin(), J);
+    firstJ++;
+    secondJ++;
+
     tree.listAdj[firstJ].push_back(0);
     tree.listAdj[secondJ].push_back(0);
-    tree.listAdj.insert(tree.listAdj.begin(), J);
+
     return tree;
 }
 
 int main(int argc, char **argv)
 {
     auto data = Data(argc, argv[1]);
+    data.read();
+    cout << "Dimensão: " << data.getDimension() << endl;
     Tree tree = Make1_Tree(data);
     for (int i = 0; i < data.getDimension(); i++)
     {
