84c24eda844a82b4a33b92a2a50bd1c03db061fe
Adriane Lopes <adriane@q7.logq.int>
2025-08-11 17:48:03 -0300

    comentarios

--- Relaxação Lagrangiana/src/main.cpp
+++ Relaxação Lagrangiana/src/main.cpp
@@ -30,10 +30,10 @@ vector<vector<double>> modifica_Matriz(Data *data)
 Tree Make1_Tree(Data &data)
 {
     vector<vector<double>> matriz = modifica_Matriz(&data);
-    auto kruskal = Kruskal(matriz);
-    double inicost = kruskal.MST(data.getDimension() - 1);
+    auto kruskal = Kruskal(matriz);                        // Guarda os pares e seu custos
+    double inicost = kruskal.MST(data.getDimension() - 1); // Constrói a MST sem considerar o primeiro  vértice e guarda seu custo
     Tree tree;
-    tree.listAdj = kruskal.getEdges();
+    tree.listAdj = kruskal.getEdges(); // Guarda a árvore
 
     cout << "Tamanho listAdj: " << tree.listAdj.size() << endl;
 
@@ -43,13 +43,12 @@ Tree Make1_Tree(Data &data)
     int firstJ = 0;
     int secondJ;
 
-    cout << "firstJ: " << firstJ << ", secondJ: " << secondJ << endl;
-
+    // Procura os vértices mais "baratos" para ligar ao vértice inicial
     for (int j = 2; j < data.getDimension(); j++)
     {
 
         cost = data.getDistance(1, j);
-        cout << "Cost: " << cost << endl;
+        // cout << "Cost: " << cost << endl;
 
         if (cost < first)
         {
@@ -69,6 +68,8 @@ Tree Make1_Tree(Data &data)
     cout << "firstJ: " << firstJ << ", secondJ: " << secondJ << endl;
 
     tree.cost = inicost + first + second;
+    cout << "Custo da árvore: " << tree.cost << endl;
+
     vector<int> J = {firstJ, secondJ};
     tree.listAdj.insert(tree.listAdj.begin(), J);
     tree.listAdj[firstJ].push_back(0);
@@ -77,41 +78,53 @@ Tree Make1_Tree(Data &data)
     return tree;
 }
 
-Tree Subgradient(Data &data)
+/* Tree Subgradient(Data &data)
 {
-    /*  vector<double> λ, bestλ;
-     long double ε = 1;
-     long double εmin = 10;
-     double wbest = 0;
-     int k = 0;
-     while ()
-     {
-         Tree tree = Make1_Tree(data);
-         for (int i = 0; i < data.getDimension(); i++)
-         {
-             cout << i << " | ";
-             for (int &a : tree.listAdj[i])
-             {
-                 cout << a << " - ";
-             }
-             cout << endl;
-         }
-         double w = tree.cost;
-         if (w > wbest)
-         {
-             wbest = w;
-             bestλ = λ;
-             k = 0;
-         }
-         else{
-         k = k + 1;
-
+    vector<double> λ, bestλ;
+    long double ε = 1;
+    long double εmin = 10;
+    double wbest = 0;
+    int k = 0;
+    while ()
+    {
+        Tree tree = Make1_Tree(data);
+        for (int i = 0; i < data.getDimension(); i++)
+        {
+            cout << i << " | ";
+            for (int &a : tree.listAdj[i])
+            {
+                cout << a << " - ";
+            }
+            cout << endl;
         }
-     } */
-}
+        double w = tree.cost;
+        if (w > wbest)
+        {
+            wbest = w;
+            bestλ = λ;
+            k = 0;
+        }
+        else
+        {
+            k = k + 1;
+        }
+    }
+} */
 
 int main(int argc, char **argv)
 {
     auto data = Data(argc, argv[1]);
     data.read();
+    Tree tree = Make1_Tree(data);
+    for (int i = 0; i < data.getDimension(); i++)
+    {
+        cout << i << " | ";
+        for (int &a : tree.listAdj[i])
+        {
+            cout << a << " - ";
+        }
+        cout << endl;
+    }
+    double w = tree.cost;
+    cout << "Custo da árvore: " << w << endl;
 }
