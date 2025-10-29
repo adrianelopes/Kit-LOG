87a275f585b8b9fc5f39aad431cf38f0104c27fa
Adriane Lopes <adrianelf04@gmail.com>
2025-10-29 20:17:23 -0300

    ajustando matriz

--- Relaxação Lagrangiana/src/main.cpp
+++ Relaxação Lagrangiana/src/main.cpp
@@ -15,7 +15,6 @@ struct Tree
 vector<vector<double>> modifica_Matriz(Data *data)
 {
     int dim = data->getDimension() - 1;
-    // cout << "DIM: " << dim << endl;
     vector<vector<double>> matriz(dim, vector<double>(dim));
     for (int i = 2; i <= data->getDimension(); i++)
     {
@@ -27,6 +26,17 @@ vector<vector<double>> modifica_Matriz(Data *data)
     return matriz;
 }
 
+vector<vector<double>> altera_matriz(Data data, vector<double> lambda)
+{
+
+    for (int i = 0; i < data.getDimension(); i++)
+    {
+        for (int j = 0; j < data.getDimension(); j++)
+        {
+            data.distMatrix[i][j] = data.distMatrix[i][j] - lambda[i + 1] - lambda[j + 1];
+        }
+    }
+}
 Tree Make1_Tree(Data &data, vector<double> lambda)
 {
     vector<vector<double>> matriz = modifica_Matriz(&data);
@@ -35,46 +45,17 @@ Tree Make1_Tree(Data &data, vector<double> lambda)
     Tree tree;
     tree.listAdj = kruskal.getEdges();
 
-    cout << "Lambda dentro da make_tree" << endl;
-    for (int i = 0; i < data.getDimension(); i++)
-    {
-        cout << "Lambda: " << i << "valor: " << lambda[i] << endl;
-    }
-
-    cout << "Antes: " << endl;
-    cout << "[2][1]: " << data.distMatrix[2][1] << endl;
-
     // Alterar  matriz distância
-    for (int l = 1; l < lambda.size(); l++)
-    {
-        for (int j = 0; j < data.getDimension(); j++)
-        {
-            for (int i = 0; i < data.getDimension(); i++)
-            {
-                data.distMatrix[i][j] = data.distMatrix[i][j] - lambda[l];
-                // cout << "I: " << i << " J: " << j << " Valor: " << data.distMatrix[i][j] << endl;
-            }
-        }
-    }
-    cout << "Depois: " << endl;
-
-    cout << "[2][1]: " << data.distMatrix[2][1] << endl;
-
-    // cout << "Tamanho listAdj: " << tree.listAdj.size() << endl;
-
     double cost;
     double second = numeric_limits<double>::infinity();
     double first = numeric_limits<double>::infinity();
     int firstJ = 0;
     int secondJ;
 
-    // cout << "firstJ: " << firstJ << ", secondJ: " << secondJ << endl;
-
     for (int j = 2; j < data.getDimension(); j++)
     {
 
         cost = data.getDistance(1, j);
-        // cout << "Cost: " << cost << endl;
 
         if (cost < first)
         {
@@ -90,8 +71,6 @@ Tree Make1_Tree(Data &data, vector<double> lambda)
             secondJ = j;
         }
     }
-
-    // cout << "firstJ: " << firstJ << ", secondJ: " << secondJ << endl;
 
     tree.cost = inicost + first + second;
     vector<int> J = {firstJ, secondJ};
@@ -109,37 +88,39 @@ Tree Subgradient(Data &data, double UB)
     double wbest = 0, w = 0, mi = 0, sum = 0;
     int k = 0, kmax = 30, degree = 0;
     bool condition = true;
-    Tree best_tree, tree;
+    Tree best_tree, tree, Original_tree;
     int count = 0;
     for (int i = 0; i < data.getDimension(); i++)
     {
         lambda[i] = 0;
     }
 
-    // eps < epsmin & condition == true
-    while (count != 2)
+    Original_tree = Make1_Tree(data, lambda);
+
+    cout << "Tree criada " << endl;
+
+    for (int i = 0; i < data.getDimension(); i++)
     {
-        /*   cout << "----------------------------------------------" << endl;
-          for (int i = 0; i < data.getDimension(); i++)
-          {
-              cout << "Lambda: " << i << "valor: " << lambda[i] << endl;
-          } */
+        cout << i << " | ";
+        for (int &a : Original_tree.listAdj[i])
+        {
+            cout << a << " - ";
+        }
+        cout << endl;
+    }
+
+    // eps < epsmin & condition == true
+    while (count != 3)
+    {
+        cout << "----------------------------------------------" << endl;
+        for (int i = 0; i < data.getDimension(); i++)
+        {
+            cout << "Lambda: " << i << "valor: " << lambda[i] << endl;
+        }
 
         tree = Make1_Tree(data, lambda);
         condition = false;
 
-        /*  cout << "Tree criada " << endl;
-
-         for (int i = 0; i < data.getDimension(); i++)
-         {
-             cout << i << " | ";
-             for (int &a : tree.listAdj[i])
-             {
-                 cout << a << " - ";
-             }
-             cout << endl;
-         } */
-        cout << "Custo da tree atual: " << tree.cost << endl;
         w = tree.cost;
         if (w > wbest)
         {
@@ -161,7 +142,7 @@ Tree Subgradient(Data &data, double UB)
         for (int i = 1; i < data.getDimension(); i++)
         {
 
-            degree = tree.listAdj[i].size();
+            degree = Original_tree.listAdj[i].size();
             if (degree != 2)
             {
                 condition = true;
@@ -173,7 +154,7 @@ Tree Subgradient(Data &data, double UB)
 
         for (int i = 1; i < data.getDimension(); i++)
         {
-            degree = tree.listAdj[i].size();
+            degree = Original_tree.listAdj[i].size();
             lambda[i] = lambda[i] + (mi * (2 - degree));
         }
         count++;
@@ -187,19 +168,19 @@ int main(int argc, char **argv)
     auto data = Data(argc, argv[1]);
     data.read();
 
-    Tree best_tree = Subgradient(data, 500000);
-    /*
-        cout << "Tree final: " << endl;
+    Tree best_tree = Subgradient(data, 148);
 
-        for (int i = 0; i < data.getDimension(); i++)
+    cout << "Tree final: " << endl;
+
+    for (int i = 0; i < data.getDimension(); i++)
+    {
+        cout << i << " | ";
+        for (int &a : best_tree.listAdj[i])
         {
-            cout << i << " | ";
-            for (int &a : best_tree.listAdj[i])
-            {
-                cout << a << " - ";
-            }
-            cout << endl;
+            cout << a << " - ";
         }
+        cout << endl;
+    }
 
-        cout << "Custo: " << best_tree.cost << endl; */
+    cout << "Custo: " << best_tree.cost << endl;
 }
